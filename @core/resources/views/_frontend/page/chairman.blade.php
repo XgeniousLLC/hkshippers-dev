548adac01f0a8749bccac4afa4a4be089fcfad99
DiptoSarkar182 <diptomanchester2018@gmail.com>
2026-08-13 11:37:45 +0600

    fix chairman name two line

--- @core/resources/views/_frontend/page/chairman.blade.php
+++ @core/resources/views/_frontend/page/chairman.blade.php
@@ -9,10 +9,10 @@
     <div class="content-wrapper ">
         <section class="content-block chairman" >
             <div class="container" style="border-bottom:none;">
-                
+
                 <div class="row">
                     <div class="col-md-12" style="display:flex; justify-content:center;">
-                        <div style="width:100%; max-width:600px;">
+                        <div class="chairman-list" style="width:100%; max-width:900px;">
 
                         @foreach($items as $item)
                             @php
@@ -22,15 +22,15 @@
                                     $img = image_resize($bg_img['img_url'], 200, 266);
                                 }
                             @endphp
-                            <div style="display:flex; align-items:flex-end; gap:2rem; margin-bottom:3rem;">
-                                <div style="flex:0 0 200px;">
+                            <div class="chairman-item">
+                                <div class="chairman-item-photo">
                                     @if($img)
                                         <img src="{{$img}}" style="width:200px; display:block;">
                                     @endif
                                 </div>
-                                <div style="flex:1; padding-top:0.5rem;">
+                                <div class="chairman-item-info">
                                     @if($item->name)
-                                        <h4 style="margin-bottom:0.5rem;">{{$item->name}}</h4>
+                                        <h4 class="chairman-item-name">{{$item->name}}</h4>
                                     @endif
                                     @if($item->title)
                                         <p style="margin:0;">{{$item->title}}</p>
@@ -45,7 +45,42 @@
             </div>
         </section>
     </div>
-        
+
+    <style>
+        .chairman-item {
+            display: flex;
+            align-items: flex-end;
+            gap: 2rem;
+            margin-bottom: 3rem;
+        }
+        .chairman-item-photo {
+            flex: 0 0 200px;
+        }
+        .chairman-item-info {
+            flex: 1;
+            padding-top: 0.5rem;
+            min-width: 0;
+        }
+        .chairman-item-name {
+            margin-bottom: 0.5rem;
+            white-space: nowrap;
+        }
+        @media (max-width: 767px) {
+            .chairman-item {
+                flex-direction: column;
+                align-items: center;
+                text-align: center;
+                gap: 0.75rem;
+            }
+            .chairman-item-photo {
+                flex: 0 0 auto;
+            }
+            .chairman-item-name {
+                white-space: normal;
+            }
+        }
+    </style>
+
     <!--/main content wrapper -->
 
 @endsection
